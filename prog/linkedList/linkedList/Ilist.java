6f6fc21c0e0743e4578514096f43dbdb0913355a
maelperot <mael.perot@etu.univ-tours.fr>
2024-11-13 15:28:04 +0100

    feat: suite TP6 prog

--- prog/linkedList/linkedList/Ilist.java
+++ prog/linkedList/linkedList/Ilist.java
@@ -4,15 +4,15 @@ import java.util.Iterator;
 
 public interface Ilist<T> extends Iterable<T>{
     public void add(T elem);
-    public boolean add(T elem, int index);
+    public boolean add(T elem, int index) throws Exception;
     public void clear();
     public boolean contains(T elem);
     public boolean equals(Object other);
-    public T get(int index);
+    public T get(int index) throws Exception;
     public int indexOf(T elem);
     public boolean isEmpty();
     public Iterator<T> Iterator();
-    public T remove(int index);
+    public T remove(int index) throws Exception;
     public T set(T elem, int index);
     public int size();
 }
